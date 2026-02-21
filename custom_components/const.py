bb7a79cc79b76e5a5450372771d1e182d2be0f3a
deangoldhill <deangoldhill@gmail.com>
2026-02-21 17:54:55 +0000

    Create const.py

---
+++ custom_components/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "checkpoint_gaia"
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_PORT = "port"
+CONF_VERIFY_SSL = "verify_ssl"
+CONF_API_VERSION = "api_version"
+
+DEFAULT_PORT = 443
+DEFAULT_API_VERSION = "v1.8"
+DEFAULT_SCAN_INTERVAL = 30
